e87e44d7561a72aeedb709ec4b068d62502a28bb
Marcin Jekot <marcin@jekot.net>
2016-05-12 06:18:15 +0200

    JBaaS: Jeff Bezos as a Service /random/quotes

---
+++ quotes.js
@@ -0,0 +1,27 @@
+'use strict'
+
+module.exports = [
+  "Your margin is my opportunity.",
+  "The balance of power is shifting toward consumers and away from companies… The right way to respond to this if you are a company is to put the vast majority of your energy, attention and dollars into building a great product or service and put a smaller amount into shouting about it, marketing it.",
+  "If you double the number of experiments you do per year you’re going to double your inventiveness.",
+  "If you decide that you’re going to do only the things you know are going to work, you’re going to leave a lot of opportunity on the table.",
+  "You have to be willing to be misunderstood if you’re going to innovate.",
+  "Frugality drives innovation, just like other constraints do. One of the only ways to get out of a tight box is to invent your way out.",
+  "The great thing about fact-based decisions is that they overrule the hierarchy.",
+  "A company shouldn't get addicted to being shiny, because shiny doesn't last.",
+  "I think frugality drives innovation, just like other constraints do. One of the only ways to get out of a tight box is to invent your way out.",
+  "If you're competitor-focused, you have to wait until there is a competitor doing something. Being customer-focused allows you to be more pioneering.",
+  "Part of company culture is path-dependent—it's the lessons you learn along the way.",
+  "There'll always be serendipity involved in discovery.",
+  "If you never want to be criticized, for goodness' sake don't do anything new.",
+  "I strongly believe that missionaries make better products. They care more. For a missionary, it's not just about the business. There has to be a business, and the business has to make sense, but that's not why you do it. You do it because you have something meaningful that motivates you.",
+  "Life's too short to hang out with people who aren't resourceful.",
+  "The one thing that offends me the most is when I walk by a bank and see ads trying to convince people to take out second mortgages on their home so they can go on vacation. That's approaching evil.",
+  "We've had three big ideas at Amazon that we've stuck with for 18 years, and they're the reason we're successful: Put the customer first. Invent. And be patient.",
+  "All businesses need to be young forever. If your customer base ages with you, you're Woolworth's.",
+  "If you're not stubborn, you'll give up on experiments too soon. And if you're not flexible, you'll pound your head against the wall and you won't see a different solution to a problem you're trying to solve.",
+  "In the old world, you devoted 30% of your time to building a great service and 70% of your time to shouting about it. In the new world, that inverts.",
+  "There are two kinds of companies, those that work to try to charge more and those that work to charge less. We will be the second.",
+  "I would never say no to something the team wanted to do, but I might say yes to something the team didn't want to do. You want there to be multiple ways to get to 'yes' because you want to encourage risk-taking.",
+  "You can have the best technology, you can have the best business model, but if the storytelling isn't amazing, it won't matter. Nobody will watch.",
+]
--- service.js
+++ service.js
@@ -4,19 +4,35 @@ const Promise = require('bluebird')
 const express = require('express')
 const {exec} = require('child_process')
 const {version} = require('./package.json')
+const quotes = require('./quotes')
 const app = express()
+
+app.use('/', (req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+})
 
 app.route('/health')
   .get((req, res) => res.send('ok'))
 
-
-
+app.route('/quotes/random')
+  .get((req, res) => Promise.fromCallback(cb => exec('hostname', cb))
+    .delay(200)
+    .then(hostname => {
+      res.json({
+        quote: quotes[Math.floor(Math.random() * quotes.length)],
+        que: '121',
+        what: 'service-1',
+        when: new Date(),
+        hostname,
+        version,
+      })
+    })
+  )
 
 app.use('/', (req, res) => {
-  console.log(`${req.method} ${req.url}`)
-
   Promise.fromCallback(cb => exec('hostname', cb))
-  // .delay(200)
+  .delay(200)
   .then(hostname => hostname.trim())
   .then(hostname => res.json({
     hostname,
