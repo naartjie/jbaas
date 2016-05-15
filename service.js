ef6fceddaa033b28a87081b40e5f3eb7733382d4
Marcin Jekot <marcin@jekot.net>
2016-05-15 16:22:02 +0200

    go back to hostname for host info

--- service.js
+++ service.js
@@ -14,16 +14,16 @@ app.use('/', (req, res, next) => {
 app.route('/health').get((req, res) => res.send('ok'))
 
 app.use('/', (req, res) => {
-  getIP()
+  getHostInfo()
   .delay(200)
-  .then(ip => {
+  .then(host => {
 
     let quote = 'Word. ' + quotes[Math.floor(Math.random() * quotes.length)]
     let {trim} = req.query
 
     if (trim) quote = `${quote.slice(0, trim)}...`
 
-    res.json({ ip, quote })
+    res.json({ host, quote })
   })
 })
 
@@ -31,7 +31,8 @@ app.listen(3000, () => {
   console.log('service-1 started on port 3000')
 })
 
-function getIP() {
-  const cmd = '/sbin/ifconfig eth0 | grep \'inet addr:\' | cut -d: -f2 | awk \'{ print $1}\''
+function getHostInfo() {
+  // const cmd = '/sbin/ifconfig eth0 | grep \'inet addr:\' | cut -d: -f2 | awk \'{ print $1}\''
+  const cmd = 'hostname'
   return Promise.fromCallback(cb => exec(cmd, cb)).then(ip => ip.trim())
 }
